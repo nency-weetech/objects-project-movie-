1059fc69ff558a03a7b8d53e6f45323bb55ed565
nency rana <nency@weetechsolution.com>
2026-06-25 09:54:26 +0530

    create 'add movie handler' fumcion

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,1 +1,32 @@
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
 
+
+const movie = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info : {
+        title,
+        [extraName] : extraValue
+    },
+    id : Math.random()
+  }
+
+  movie.push(newMovie);
+  console.log(newMovie);
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
